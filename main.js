6cb6f12f1b4b8a9027084f705bc12760761b9aee
Janine Lopes <janineeng.pro@gmail.com>
2023-02-02 16:18:45 -0300

    alterações

--- main.js
+++ main.js
@@ -10,3 +10,26 @@ function mensagemCliente(){
     alert ('Estamos felizes por escolher os serviços Eng_Pro_|, estamos sempre diponiveis para mais esclarecimentos')
 }
 
+const somar = document.querySelector("#somar")
+const subtrair = document.querySelector("#subtrair")
+const comprar = document.querySelector("#comprar")
+
+const controle = document.querySelectorAll(".botton")
+
+controle.forEach( (elemento)=> {
+    elemento.addEventListener("click", (evento) => {
+        manipulaDados(evento.target.textContent, evento.target.parentNode)
+    })
+})
+function manipulaDados(operacao, controle) {
+    const servico = controle.querySelector(".controle-contador")
+
+    if(operacao === "-"){
+        servico.value =  parseInt(servico.value) -1
+    }
+    else{
+        servico.value =  parseInt(servico.value) +1
+    }
+}
+
+
