2e83cf529520489763027027037f6bd5bd3edc1a
Janine Lopes <janineeng.pro@gmail.com>
2023-02-03 11:43:06 -0300

    Finalizando o primeiro aula de java

--- main.js
+++ main.js
@@ -11,18 +11,31 @@ function mensagemCliente(){
 }
 
 const controle = document.querySelectorAll("[data-controle]")
+const carrinho = document.querySelectorAll("[data-carrinho]")
+const servico = {
+    "td": {
+        "carrinho": 1
+    },
+    "aa": {
+        "carrinho": 1
+    },
+    "cp": {
+        "carrinho": 1
+    }
+}
+console.log(servico)
+
 // querySelectorAll Criou uma ARRAY com elemento do mesmo dataSet
 
 controle.forEach( (elemento)=> {
     elemento.addEventListener("click", (evento) => {
         manipulaDados(evento.target.dataset.controle, evento.target.parentNode)
+        atualizaCarrinho(evento.target.dataset.servicos)
     })
 })
         //forEach paraCada() - executar determinada função para cada elemento de um array em ordem ascendente.
         //SINTAXE- array.forEach("funcao" (elemento [, indice [, array]])[, thisArgumento])
         //evento com alvo no dataset do filho controle e evento com alvo elemento pai serviço
-
-
 
 function manipulaDados(operacao, controle) {
     const servico = controle.querySelector("[data-contador]")
@@ -38,4 +51,16 @@ function manipulaDados(operacao, controle) {
     }
 }
 
+function atualizaCarrinho (servicos){
+    console.log(servico[servicos])
+    
+        carrinho.forEach( (elemento) => {
+            console.log(elemento.textContent)
+            console.log(elemento.dataset.carrinho)
+            elemento.textContent = parseInt(elemento.textContent) + servico[servicos][elemento.dataset.carrinho]
+        })
+    }
+       
 
+
+   
