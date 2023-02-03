711b5f9967d53c126d9d82c365cd9745bfe34eb4
JanineEngPro <117129153+JanineEngPro@users.noreply.github.com>
2023-02-03 16:33:55 -0300

    java pronto

--- main1.js
+++ main1.js
@@ -1,49 +1,81 @@
 const form = document.getElementById("novoItem")
-//a variável  constante 'form'recebe os elementos buscado pelo Get com class "novoItem" 
 const lista = document.getElementById("lista")
-// a variável constante 'lista" recebe os elementos buscado pelo get com a class "lista"
-// ???essa lista esta vazia e quero faze com que receba os item dos input para isso tem que criar uma 
-// function que cria elemento dentro do html. ???
+const itens = JSON.parse(localStorage.getItem("itens")) || []
 
-//criamos novos iten dentro do html
+itens.forEach( (elemento) => {
+    criaElemento(elemento)
+} )
+
 form.addEventListener("submit", (evento) => {
     evento.preventDefault()
 
-    criaElemento(evento.target.elements['nome'].value, evento.target.elements['quantidade'].value)
-    
-    evento.target.elements['nome'].value = ""//Para deixar o input vazio
-    evento.target.elements['quantidade'].value = ""//Para deixar o input vazio
-})
+    const nome = evento.target.elements['nome']
+    const quantidade = evento.target.elements['quantidade']
 
+    const existe = itens.find( elemento => elemento.nome === nome.value )
 
-// o submit envia para o form os itens que o input recebe
-// evento.preventDefault() - interrompe o comportamento padrão do evento de enviar para página para poder enviar para o form
-
-function criaElemento(nome, quantidade) {
-    const novoItem = document.createElement('li')//criou um tag li e salvou em novoItem
-    novoItem.classList.add("item") //no novoItem adicionou uma class "item"
-
-    const numeroItem = document.createElement('strong')//criou a tag strong
-    numeroItem.innerHTML = quantidade //numeroItem recebe (pelo innerHTML) o elemento quantidade 
-    novoItem.appendChild(numeroItem)//anexar filho 
-
-    novoItem.innerHTML += nome
-
-    lista.appendChild(novoItem)
-
-
-//??????? Armazenar as informações na maquina os itens criado dentro do html?????
-// localStorage
-
-
-const itemAtual = {
-    "nome": nome,
-    "quantidade": quantidade
+    const itemAtual = {
+        "nome": nome.value,
+        "quantidade": quantidade.value
     }
 
-itens.push(itemAtual)
+    if (existe) {
+        itemAtual.id = existe.id
+        
+        atualizaElemento(itemAtual)
 
-localStorage.setItem("item", JOSON.stringify(itens))
+        itens[itens.findIndex(elemento => elemento.id === existe.id)] = itemAtual
+    } else {
+        itemAtual.id = itens[itens.length -1] ? (itens[itens.length-1]).id + 1 : 0;
+
+        criaElemento(itemAtual)
+
+        itens.push(itemAtual)
+    }
+
+    localStorage.setItem("itens", JSON.stringify(itens))
+
+    nome.value = ""
+    quantidade.value = ""
+})
+
+function criaElemento(item) {
+    const novoItem = document.createElement("li")
+    novoItem.classList.add("item")
+
+    const numeroItem = document.createElement("strong")
+    numeroItem.innerHTML = item.quantidade
+    numeroItem.dataset.id = item.id
+    novoItem.appendChild(numeroItem)
+    
+    novoItem.innerHTML += item.nome
+
+    novoItem.appendChild(botaoDeleta(item.id))
+
+    lista.appendChild(novoItem)
+}
+
+function atualizaElemento(item) {
+    document.querySelector("[data-id='"+item.id+"']").innerHTML = item.quantidade
+}
+
+function botaoDeleta(id) {
+    const elementoBotao = document.createElement("button")
+    elementoBotao.innerText = "X"
+
+    elementoBotao.addEventListener("click", function() {
+        deletaElemento(this.parentNode, id)
+    })
+
+    return elementoBotao
+}
+
+function deletaElemento(tag, id) {
+    tag.remove()
+
+    itens.splice(itens.findIndex(elemento => elemento.id === id), 1)
+
+    localStorage.setItem("itens", JSON.stringify(itens))
 }
 
 
